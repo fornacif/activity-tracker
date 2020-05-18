16732b678cd211a41517e9ebd3ae2db644cee37e
fornacia <fornacia@192.168.0.28>
2020-05-18 09:02:00 +0200

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,68 @@
 <template>
-  <v-app id="app">
-    <v-navigation-drawer v-model="drawer" app>
-      <v-list dense>
-        <v-list-item to="/">
-          <v-list-item-action>
-            <v-icon>mdi-home-analytics</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Dashboard</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item to="/map">
-          <v-list-item-action>
-            <v-icon>mdi-google-maps</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Map</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-divider/>
-        <v-list-item to="/activity">
-          <v-list-item-action>
-            <v-icon>mdi-table-large-plus</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Activity</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item to="/activities">
-          <v-list-item-action>
-            <v-icon>mdi-table-large</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Activities</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-divider/>
-        <v-list-item to="/profile">
-          <v-list-item-action>
-            <v-icon>mdi-account</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Profile</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-app-bar app color="blue-grey darken-3" dark>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-toolbar-title>Activity Tracker - 2.0.0</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn icon to="/logout">
-        <v-icon>mdi-logout</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <v-container fluid>
-        <router-view/>
-      </v-container>
-    </v-content>
-  </v-app>
+   <v-app id="app">
+      <v-navigation-drawer v-model="drawer" app>
+         <v-list dense>
+            <v-list-item to="/">
+               <v-list-item-action>
+                  <v-icon>mdi-home-analytics</v-icon>
+               </v-list-item-action>
+               <v-list-item-content>
+                  <v-list-item-title>Dashboard</v-list-item-title>
+               </v-list-item-content>
+            </v-list-item>
+            <v-list-item to="/map">
+               <v-list-item-action>
+                  <v-icon>mdi-google-maps</v-icon>
+               </v-list-item-action>
+               <v-list-item-content>
+                  <v-list-item-title>Map</v-list-item-title>
+               </v-list-item-content>
+            </v-list-item>
+            <v-divider/>
+            <v-list-item to="/activity">
+               <v-list-item-action>
+                  <v-icon>mdi-table-large-plus</v-icon>
+               </v-list-item-action>
+               <v-list-item-content>
+                  <v-list-item-title>Activity</v-list-item-title>
+               </v-list-item-content>
+            </v-list-item>
+            <v-list-item to="/activities">
+               <v-list-item-action>
+                  <v-icon>mdi-table-large</v-icon>
+               </v-list-item-action>
+               <v-list-item-content>
+                  <v-list-item-title>Activities</v-list-item-title>
+               </v-list-item-content>
+            </v-list-item>
+            <v-divider/>
+            <v-list-item to="/profile">
+               <v-list-item-action>
+                  <v-icon>mdi-account</v-icon>
+               </v-list-item-action>
+               <v-list-item-content>
+                  <v-list-item-title>Profile</v-list-item-title>
+               </v-list-item-content>
+            </v-list-item>
+         </v-list>
+      </v-navigation-drawer>
+      <v-app-bar app color="blue-grey darken-3" dark>
+         <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+         <v-toolbar-title>Activity Tracker - 2.0.1</v-toolbar-title>
+         <v-spacer></v-spacer>
+         <v-btn icon to="/logout">
+            <v-icon>mdi-logout</v-icon>
+         </v-btn>
+      </v-app-bar>
+      <v-content>
+         <v-alert color="green" dense dark v-show="updateExists">
+            New version available <v-btn small color="green darken-3" dark @click="refreshUI()">Refresh</v-btn>
+         </v-alert>
+         <v-container fluid>
+            <router-view/>
+         </v-container>
+      </v-content>
+   </v-app>
 </template>
 
 <script>
--- src/mixin.js
+++ src/mixin.js
@@ -1,7 +1,21 @@
 import Vue from 'vue'
 
 Vue.mixin({
+	data() {
+		return {
+			updateExists: false,
+		};
+	},
+	created() {
+		document.addEventListener('serviceWorkerUpdated', this.showRefreshUI, { once: true });
+	},
 	methods: {
+		showRefreshUI() {
+			this.updateExists = true;
+		},
+		refreshUI() {
+			window.location.reload();
+		},
 		durationToTime(duration) {
 			let minutes = Math.round(duration * 60);
 			return this.$moment().startOf('day').add(minutes, 'minutes').format('HH:mm');
